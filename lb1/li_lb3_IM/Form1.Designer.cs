60d5d3f4f095e828498902643b1f9b7d10e95437
meme0w <ialina20021@gmail.com>
2023-03-05 23:55:54 +0700

    сделана 3лб

---
+++ lb1/li_lb3_IM/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace li_lb3_IM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.acriveBut = new System.Windows.Forms.Button();
+            this.edRule = new System.Windows.Forms.NumericUpDown();
+            this.labelRule = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label0 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).BeginInit();
+            this.table.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // acriveBut
+            // 
+            this.acriveBut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.acriveBut.Location = new System.Drawing.Point(67, 101);
+            this.acriveBut.Margin = new System.Windows.Forms.Padding(2);
+            this.acriveBut.Name = "acriveBut";
+            this.acriveBut.Size = new System.Drawing.Size(180, 47);
+            this.acriveBut.TabIndex = 7;
+            this.acriveBut.Text = "Старт";
+            this.acriveBut.UseVisualStyleBackColor = true;
+            this.acriveBut.Click += new System.EventHandler(this.acriveBut_Click);
+            // 
+            // edRule
+            // 
+            this.edRule.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.edRule.Location = new System.Drawing.Point(150, 50);
+            this.edRule.Margin = new System.Windows.Forms.Padding(2);
+            this.edRule.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.edRule.Name = "edRule";
+            this.edRule.Size = new System.Drawing.Size(97, 26);
+            this.edRule.TabIndex = 6;
+            this.edRule.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.edRule.ValueChanged += new System.EventHandler(this.edRule_ValueChanged);
+            // 
+            // labelRule
+            // 
+            this.labelRule.AutoSize = true;
+            this.labelRule.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelRule.Location = new System.Drawing.Point(62, 51);
+            this.labelRule.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelRule.Name = "labelRule";
+            this.labelRule.Size = new System.Drawing.Size(67, 25);
+            this.labelRule.TabIndex = 5;
+            this.labelRule.Text = "Rule:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(5, 338);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 26);
+            this.label12.TabIndex = 12;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(5, 310);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(34, 26);
+            this.label11.TabIndex = 11;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(5, 282);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(34, 26);
+            this.label10.TabIndex = 10;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(5, 254);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 26);
+            this.label9.TabIndex = 9;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(5, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 26);
+            this.label8.TabIndex = 8;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(5, 198);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 26);
+            this.label7.TabIndex = 7;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(5, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 26);
+            this.label6.TabIndex = 6;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(5, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 26);
+            this.label5.TabIndex = 5;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label0.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label0.Location = new System.Drawing.Point(5, 2);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(34, 26);
+            this.label0.TabIndex = 0;
+            this.label0.Text = "0";
+            this.label0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label0.Click += new System.EventHandler(this.label0_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(5, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 26);
+            this.label4.TabIndex = 4;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(5, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 26);
+            this.label3.TabIndex = 3;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(5, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 26);
+            this.label2.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(5, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 26);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // table
+            // 
+            this.table.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Outset;
+            this.table.ColumnCount = 15;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.865659F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.373429F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.443148F));
+            this.table.Controls.Add(this.label1, 0, 1);
+            this.table.Controls.Add(this.label2, 0, 2);
+            this.table.Controls.Add(this.label3, 0, 3);
+            this.table.Controls.Add(this.label4, 0, 4);
+            this.table.Controls.Add(this.label0, 0, 0);
+            this.table.Controls.Add(this.label5, 0, 5);
+            this.table.Controls.Add(this.label6, 0, 6);
+            this.table.Controls.Add(this.label7, 0, 7);
+            this.table.Controls.Add(this.label8, 0, 8);
+            this.table.Controls.Add(this.label9, 0, 9);
+            this.table.Controls.Add(this.label10, 0, 10);
+            this.table.Controls.Add(this.label11, 0, 11);
+            this.table.Controls.Add(this.label12, 0, 12);
+            this.table.Location = new System.Drawing.Point(293, 41);
+            this.table.Name = "table";
+            this.table.RowCount = 13;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.692308F));
+            this.table.Size = new System.Drawing.Size(438, 366);
+            this.table.TabIndex = 0;
+            this.table.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.table_CellPaint);
+            this.table.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.acriveBut);
+            this.Controls.Add(this.edRule);
+            this.Controls.Add(this.labelRule);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edRule)).EndInit();
+            this.table.ResumeLayout(false);
+            this.table.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button acriveBut;
+        private System.Windows.Forms.NumericUpDown edRule;
+        private System.Windows.Forms.Label labelRule;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel table;
+    }
+}
+
---
+++ lb1/li_lb3_IM/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace li_lb3_IM
+{
+    public partial class Form1 : Form
+    {
+
+        bool help = true;
+        List<Control> labels = new List<Control>(); //создаем список элементов управления - лэйблов, для нумерации строк
+        string biNumber; //переменная для ддвоичного представления
+        List<int> FirstLine; //первая линия, которая должна быть заполнена по шаблону
+        Color[,] Colors; //массив цветов для сохранения цвета для каждой ячейки в таблице
+        List<int> line = new List<int>(new int[15]);
+        List<int> otherLine = new List<int>(new int[15]);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Colors = new Color[15, 14];
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 14; j++)
+                {
+                    Colors[i, j] = SystemColors.Control;
+                }
+            }
+        }
+
+        private void edRule_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void acriveBut_Click(object sender, EventArgs e)
+        {
+            if (help)
+            {
+                labels.Add(label0);
+                labels.Add(label1);
+                labels.Add(label2);
+                labels.Add(label3);
+                labels.Add(label4);
+                labels.Add(label5);
+                labels.Add(label6);
+                labels.Add(label7);
+                labels.Add(label8);
+                labels.Add(label9);
+                labels.Add(label10);
+                labels.Add(label11);
+                labels.Add(label12);
+
+                table.Refresh(); //таблица и её дочерние элементы управления становятся недействительными и перерисовываются
+                biNumber = Convert.ToString((int)edRule.Value, 2); //перевод числа в двоичное представление
+
+                if (biNumber.Length != 8)//если длина двоичного числа не достигает 8, то
+                {
+                    int n = 8 - biNumber.Length; //смотрим, сколько цифр не хватает
+                    for (int i = 0; i < n; i++)
+                    {
+                        biNumber = biNumber.Insert(0, "0"); //вставляем спереди нули
+                    }
+                }
+
+                //далее заполняем первую линию
+                FirstLine = new List<int>(new int[15]); //в первой линии 15 квадратиков
+                for (int i = 0; i < FirstLine.Count; i++)
+                {
+                    if (i == 7 || i == 8 || i == 12)
+                    {
+                        FirstLine[i] = 1; //квадратик заполняется цветом
+                        Colors[i, 0] = Color.Green; //красим ячейку в зеленый
+                    }
+                    else
+                    {
+                        FirstLine[i] = 0; //пустой квадратик
+                    }
+                }
+
+                for (int i = 0; i < otherLine.Count; i++)
+                {
+                    otherLine[i] = 0;
+                }
+
+                table.Refresh();
+                line = FirstLine;
+            }
+            help = false;
+            
+            if (acriveBut.Text == "Старт")
+            {
+                acriveBut.Text = "Стоп";
+                timer1.Start();
+            }
+            else
+            {
+                acriveBut.Text = "Старт";
+                timer1.Stop();
+            }
+            table.Refresh();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void label0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {   //рисуем фон ячеек
+            if (Colors != null)
+            {
+                using (var b = new SolidBrush(Colors[e.Column, e.Row]))
+                {
+                    e.Graphics.FillRectangle(b, e.CellBounds);
+                }
+            }
+        }
+
+        public int check(string help)
+        {
+            switch (help)
+            {
+                case "110":
+                    return (int)biNumber[1] - 48;
+                case "101":
+                    return (int)biNumber[2] - 48;
+                case "011":
+                    return (int)biNumber[4] - 48;
+                case "010":
+                    return (int)biNumber[5] - 48;
+                case "100":
+                    return (int)biNumber[3] - 48;
+                case "000":
+                    return (int)biNumber[7] - 48;
+                case "111":
+                    return (int)biNumber[0] - 48;
+                case "001":
+                    return (int)biNumber[6] - 48;
+                default:
+                    return (int)biNumber[0] - 48;
+            }
+        }
+
+        int Count = 1;
+        bool First = true;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labels[Count].Text = Count.ToString(); //записываем номер текущей строки в ячейку
+
+            if (First == true) //если это первая строка простодолжаем дальше
+            {
+                First = false;
+            }
+            else //если это не первая строка
+            {
+                for (int i = 0; i < otherLine.Count; i++)
+                {
+                    line[i] = otherLine[i];
+                }
+            }
+
+            for (int i = 0; i < line.Count; i++)
+            {
+                if (i != 0 && i != line.Count - 1)
+                {
+                    string Result = ""; //вспомогательная строка для считывания 3х символов
+                    Result = line[i - 1].ToString() + line[i].ToString() + line[i + 1].ToString();
+                    int result = check(Result); //провряем под какое правило подходят три считанных символа
+                    otherLine[i] = result; //записываем результат действия правила
+                    if (otherLine[i] == 1) //если 1, закрашиваем
+                    {
+                        Colors[i, Count] = Color.Green;
+                    }
+                }
+            }
+
+            table.Refresh();
+            Count++;
+
+            if (Count == 13)
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
