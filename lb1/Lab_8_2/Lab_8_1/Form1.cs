9fd9b50bafb6e5636c483426e737133c42bae096
meme0w <ialina20021@gmail.com>
2023-05-03 22:54:44 +0700

    сделала лб 8_2

---
+++ lb1/Lab_8_2/Lab_8_1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_8_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double p = 0.5;
+        double f = 0.9;
+        int k;
+        Random rnd = new Random();
+
+        private void answBut_Click(object sender, EventArgs e)
+        {
+            double value = rnd.NextDouble();
+            if (value < p)
+            {
+                k = 0;
+                answer.Text = "Стоит сто раз подумать!";
+                answer.Visible = true;
+                
+            }
+            else if (value > p && value < f)
+            {
+                k = 1;
+                answer.Text = "Обязательно!";
+                answer.Visible = true;
+            }
+            else if(value > f)
+            {
+                k = 2;
+                answer.Text = "Точно нет!";
+                answer.Visible = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            answer.Visible = false;
+        }
+    }
+}
