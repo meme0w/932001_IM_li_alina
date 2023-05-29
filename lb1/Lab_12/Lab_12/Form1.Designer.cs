c402d0651a337f28ac349dda70e971344e9e06f4
meme0w <ialina20021@gmail.com>
2023-05-29 22:36:12 +0700

    сделала лб12

---
+++ lb1/Lab_12/Lab_12/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace Lab_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startBut = new System.Windows.Forms.Button();
+            this.weatherLabel = new System.Windows.Forms.Label();
+            this.stopBut = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nmdSun = new System.Windows.Forms.NumericUpDown();
+            this.nmdCloud = new System.Windows.Forms.NumericUpDown();
+            this.nmdRain = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nmdSun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmdCloud)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmdRain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startBut
+            // 
+            this.startBut.Font = new System.Drawing.Font("Press Start 2P", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startBut.Location = new System.Drawing.Point(32, 135);
+            this.startBut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.startBut.Name = "startBut";
+            this.startBut.Size = new System.Drawing.Size(133, 34);
+            this.startBut.TabIndex = 0;
+            this.startBut.Text = "Simulate";
+            this.startBut.UseVisualStyleBackColor = true;
+            this.startBut.Click += new System.EventHandler(this.startBut_Click);
+            // 
+            // weatherLabel
+            // 
+            this.weatherLabel.AutoSize = true;
+            this.weatherLabel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.weatherLabel.Font = new System.Drawing.Font("Press Start 2P", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.weatherLabel.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.weatherLabel.Location = new System.Drawing.Point(161, 61);
+            this.weatherLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.weatherLabel.Name = "weatherLabel";
+            this.weatherLabel.Size = new System.Drawing.Size(244, 51);
+            this.weatherLabel.TabIndex = 1;
+            this.weatherLabel.Text = "Погода";
+            // 
+            // stopBut
+            // 
+            this.stopBut.Font = new System.Drawing.Font("Press Start 2P", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopBut.Location = new System.Drawing.Point(412, 135);
+            this.stopBut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.stopBut.Name = "stopBut";
+            this.stopBut.Size = new System.Drawing.Size(116, 34);
+            this.stopBut.TabIndex = 2;
+            this.stopBut.Text = "Stop";
+            this.stopBut.UseVisualStyleBackColor = true;
+            this.stopBut.Click += new System.EventHandler(this.stopBut_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(29, 229);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Солнечно:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(202, 229);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Облачно:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label3.Location = new System.Drawing.Point(373, 229);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Дождливо:";
+            // 
+            // nmdSun
+            // 
+            this.nmdSun.DecimalPlaces = 2;
+            this.nmdSun.Location = new System.Drawing.Point(98, 227);
+            this.nmdSun.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nmdSun.Name = "nmdSun";
+            this.nmdSun.Size = new System.Drawing.Size(67, 20);
+            this.nmdSun.TabIndex = 6;
+            this.nmdSun.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // nmdCloud
+            // 
+            this.nmdCloud.DecimalPlaces = 2;
+            this.nmdCloud.Location = new System.Drawing.Point(268, 227);
+            this.nmdCloud.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nmdCloud.Name = "nmdCloud";
+            this.nmdCloud.Size = new System.Drawing.Size(66, 20);
+            this.nmdCloud.TabIndex = 7;
+            this.nmdCloud.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // nmdRain
+            // 
+            this.nmdRain.DecimalPlaces = 2;
+            this.nmdRain.Location = new System.Drawing.Point(449, 227);
+            this.nmdRain.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.nmdRain.Name = "nmdRain";
+            this.nmdRain.Size = new System.Drawing.Size(80, 20);
+            this.nmdRain.TabIndex = 8;
+            this.nmdRain.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(553, 292);
+            this.Controls.Add(this.nmdRain);
+            this.Controls.Add(this.nmdCloud);
+            this.Controls.Add(this.nmdSun);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopBut);
+            this.Controls.Add(this.weatherLabel);
+            this.Controls.Add(this.startBut);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nmdSun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmdCloud)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nmdRain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBut;
+        private System.Windows.Forms.Label weatherLabel;
+        private System.Windows.Forms.Button stopBut;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nmdSun;
+        private System.Windows.Forms.NumericUpDown nmdCloud;
+        private System.Windows.Forms.NumericUpDown nmdRain;
+    }
+}
+
---
+++ lb1/Lab_12/Lab_12/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_12
+{
+    public partial class Form1 : Form
+    {
+        int[] res = new int[3];
+        Random rand = new Random();
+        string state = "sun";
+        double sunToCloud, sunToRain, cloudToSun, cloudToRain, rainToSun, rainToCloud, sunPosition, cloudPosition, rainPosition;
+        double[,] mat = new double[,]
+        {
+            { -0.4, 0.3, 0.1 },   // солнечно
+            { 0.4, -0.8, 0.4 },   // облачно
+            { 0.1, 0.4, -0.5 }    // дождливо
+        };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopBut_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            sunPosition = res[0] / k;
+            cloudPosition = res[1] / k;
+            rainPosition = res[2] / k;
+            Console.WriteLine();
+            Console.WriteLine(res[1]);
+            Console.WriteLine(res[2]);
+            Console.WriteLine(k);
+            nmdSun.Value = (decimal)sunPosition;
+            nmdCloud.Value = (decimal)cloudPosition;
+            nmdRain.Value = (decimal)rainPosition;
+        }
+
+        double k = 0;
+        int N = 50;
+
+
+        public Form1()
+        {
+          
+            InitializeComponent();
+        }
+
+        private void startBut_Click(object sender, EventArgs e)
+        {
+            k = 0;
+            res[0] = 0;res[1] = 0;res[2] = 0;
+            nmdSun.Value = 0;
+            nmdCloud.Value = 0;
+            nmdRain.Value = 0;
+            sunPosition = (double)nmdSun.Value;
+            cloudPosition = (double)nmdCloud.Value;
+            rainPosition = (double)nmdRain.Value;
+            sunToCloud = -mat[0, 1] / mat[0, 0];
+            sunToRain = -mat[0, 2] / mat[0, 0];
+            cloudToSun = -mat[1, 0] / mat[1, 1];
+            cloudToRain = -mat[1, 2] / mat[1, 1];
+            rainToSun = -mat[2, 0] / mat[2, 2];
+            rainToCloud = -mat[2, 1] / mat[2, 2];
+            weatherLabel.Text = "Солнечно";
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            k++;
+            timer1.Interval = 400;
+            double a = rand.NextDouble();
+            Console.WriteLine(a);
+            switch (state)
+            {
+                case "sun":
+                    timer1.Interval *= 1 + (int)(Math.Log(a) / mat[0, 0]);
+                    break;
+                case "cloud":
+                    timer1.Interval *= 1 + (int)(Math.Log(a) / mat[1, 1]);
+                    break;
+                case "rain":
+                    timer1.Interval *= 1 + (int)(Math.Log(a) / mat[2, 2]);
+                    break;
+            }
+
+            switch (state)
+            {
+                case "sun":
+                    if (a >= 0 && a < sunToCloud)
+                    {
+                        state = "cloud";
+                        weatherLabel.Text = "Облачно";
+                        res[1]++;
+                    }
+                    else if (a >= sunToCloud && a < 1)
+                    {
+                        state = "rain";
+                        weatherLabel.Text = "Дождливо";
+                        res[2]++;
+                    }
+                    break;
+                case "cloud":
+                    if (a >= 0 && a < cloudToSun)
+                    {
+                        state = "sun";
+                        weatherLabel.Text = "Солнечно";
+                        res[0]++;
+                    }
+                    else if (a >= cloudToSun && a < 1)
+                    {
+                        state = "rain";
+                        weatherLabel.Text = "Дождливо";
+                        res[2]++;
+                    }
+                    break;
+                case "rain":
+                    if (a >= 0 && a < rainToSun)
+                    {
+                        state = "sun";
+                        weatherLabel.Text = "Солнечно";
+                        res[0]++;
+                    }
+                    else if (a >= rainToSun && a < 1)
+                    {
+                        state = "cloud";
+                        weatherLabel.Text = "Облачно";
+                        res[1]++;
+                    }
+                    break;
+            }
+
+
+        }
+    }
+}
